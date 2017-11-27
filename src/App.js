68563915d638e996a73bd3c50708c94c5f377193
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-11-27 15:37:32 -0700

    Revised JSON request to recipe titles with buttons to select recipe

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var testString = "this is a test string.  Hello computer."
-var apiData;
-var recipes = [];
+var testString = "Hello computer.  Initialized without error... or is it...";
+var recipes;
 
 class App extends Component {
   componentDidMount(){
     var that = this;
-    var url = "http://127.0.0.1:5984/dinner_meals/_design/view3/_view/object?limit=20&reduce=false&include_docs=true&conflicts=true";
+    var url = "http://127.0.0.1:5984/dinner_meals/_design/view/_view/Recipes?limit=20&reduce=false";
     /* "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"; */
     var dataDisplay = document.getElementById('dataDisplay');
 
@@ -17,8 +16,11 @@ class App extends Component {
       .then(resp => resp.json())
       .then(function(data){
         console.log(data);
-        inputData(data);
-        dataDisplay.innerHTML = apiData;
+        recipes = recipeTitles(data);
+        var recipeList = document.getElementById('recipeList');
+        document.getElementById('recipe0').innerHTML = recipes[0];
+        document.getElementById('recipe1').innerHTML = recipes[1];
+        document.getElementById('recipe2').innerHTML = recipes[2];
       })
       .catch(function(error){
         dataDisplay.innerHTML = "Crap.... something exploded.  Ssshhhhh....  " + error;
@@ -31,11 +33,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{testString}</p>
-        <p id='dataDisplay'></p>
+        <p id='dataDisplay'>{testString}</p>
+        <div>
+          <h3 id='recipesTitle'>Recipes</h3>
+          <ul id='recipeList'>
+            <li className='recipes'><button id='recipe0'></button></li>
+            <li className='recipes'><button id='recipe1'></button></li>
+            <li className='recipes'><button id='recipe2'></button ></li>
+          </ul>
+        </div>
       </div>
     );
   }
@@ -43,21 +49,8 @@ class App extends Component {
 
 export default App;
 
-function inputData(data){
-  var rawdata = data;
+function recipeTitles(data){
+  recipes = data.rows.map(row => row.value);
 
-  var rows = rawdata.rows;
-  recipes = rows.map(row => row.value);
-  var testrec = recipes;
-
-  apiData = recipes.map(rec => recipeToString(rec));
-}
-
-function recipeToString(recipe) {
-  var result = "";
-  result += recipe.name + "</br>";
-  result += recipe.components.map(comp => comp.component);
-  result += "</br></br>";
-
-  return result;
+  return recipes;
 }
