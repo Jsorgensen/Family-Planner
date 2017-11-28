bc03b48687c30c64e5b4169a42975b8ad3bffe3c
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-11-27 22:30:53 -0700

    automated list filling.  updating html elements

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-var testString = "Hello computer.  Initialized without error... or is it...";
-var recipes;
+var display;
+var recipes = ['Wendy\'s', 'Fazoli\'s', 'Costa Vida', 'Little Ceasar\'s'];
+var recipeItems;
+createRecipeItems();
 
 class App extends Component {
   componentDidMount(){
     var that = this;
     var url = "http://127.0.0.1:5984/dinner_meals/_design/view/_view/Recipes?limit=20&reduce=false";
     /* "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"; */
-    var dataDisplay = document.getElementById('dataDisplay');
+    display = "Hello computer. Initialized without error... or did it...";
 
     fetch(url)
       .then(resp => resp.json())
-      .then(function(data){
-        console.log(data);
-        recipes = recipeTitles(data);
-        var recipeList = document.getElementById('recipeList');
-        document.getElementById('recipe0').innerHTML = recipes[0];
-        document.getElementById('recipe1').innerHTML = recipes[1];
-        document.getElementById('recipe2').innerHTML = recipes[2];
+      .then(function(json){
+        console.log(json);
+        recipes = recipeTitles(json);
+        createRecipeItems();
       })
       .catch(function(error){
-        dataDisplay.innerHTML = "Crap.... something exploded.  Ssshhhhh....  " + error;
+        display = "Crap.... something exploded.  Ssshhhhh....  " + error;
       });
+  }
+  handleClick(){
+    document.getElementById('dataDisplay').innerHTML = 'Request recipe';
   }
   render() {
     return (
@@ -33,13 +35,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p id='dataDisplay'>{testString}</p>
-        <div>
+        <p id='dataDisplay'>{display}</p>
+        <div id='recipeBlock'>
           <h3 id='recipesTitle'>Recipes</h3>
           <ul id='recipeList'>
-            <li className='recipes'><button id='recipe0'></button></li>
-            <li className='recipes'><button id='recipe1'></button></li>
-            <li className='recipes'><button id='recipe2'></button ></li>
+            {recipeItems}
           </ul>
         </div>
       </div>
@@ -52,5 +52,12 @@ export default App;
 function recipeTitles(data){
   recipes = data.rows.map(row => row.value);
 
+  document.getElementById('dataDisplay').innerHTML = recipes;
+
   return recipes;
+}
+
+function createRecipeItems(){
+  recipeItems = recipes.map((recipe) => <li className='recipes'><button>{recipe}</button></li>);
+  var ril = recipeItems;
 }
