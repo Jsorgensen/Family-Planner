6b7604c8d1c2af301f0975eda75844b7c498aad0
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-11-28 12:47:04 -0700

    implementing ReactDOM to dynamically create/add html elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,10 @@ class App extends Component {
     var url = "http://127.0.0.1:5984/dinner_meals/_design/view/_view/Recipes?limit=20&reduce=false";
     /* "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"; */
     display = "Hello computer. Initialized without error... or did it...";
+    var lis = []
+    for(var i=0; i<4; i++)
+      lis.push(<li id='test'>this is a test.... {i + 1}</li>);
+    ReactDOM.render(lis, document.getElementById('testBlock'));
 
     fetch(url)
       .then(resp => resp.json())
@@ -42,6 +47,9 @@ class App extends Component {
             {recipeItems}
           </ul>
         </div>
+        <ul id='testBlock'>
+
+        </ul>
       </div>
     );
   }
@@ -59,5 +67,9 @@ function recipeTitles(data){
 
 function createRecipeItems(){
   recipeItems = recipes.map((recipe) => <li className='recipes'><button>{recipe}</button></li>);
+  recipes.forEach(function(recipe){
+    console.log(recipe);
+    //ReactDOM.render (<li className='recipes'><button>{recipe}</button></li>, document.getElementById('recipeList'));
+  });
   var ril = recipeItems;
 }
